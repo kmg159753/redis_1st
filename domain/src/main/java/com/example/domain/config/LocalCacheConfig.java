ade9fac50c1a3671549631be5ad90cd834406181
kmg159753 <mingyugim584@gmail.com>
2025-01-19 14:37:49 +0900

    로컬 캐시 적용

---
+++ domain/src/main/java/com/example/domain/config/LocalCacheConfig.java
@@ -0,0 +1,27 @@
+package com.example.domain.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class LocalCacheConfig {
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES) // 캐시 TTL 설정
+                .maximumSize(1000); // 최대 캐시 항목 개수
+    }
+
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        return new CaffeineCacheManager("screeningCache");
+    }
+}
